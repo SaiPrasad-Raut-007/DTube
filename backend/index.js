8fdc7071df10f6f263a1376896fd8ebdbc6b7ee8
SaiPrasad-Raut-007 <saiprasad.raut.7k@gmail.com>
2026-06-25 13:31:27 +0530

    Connect to Mongo Database

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
+const MONGO_URI = 'mongodb://127.0.0.1:27017/dtube';
 const app = express();
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000!')
-});
+mongoose.connect(MONGO_URI)
+    .then(() => {
+        console.log('Success... Connected to Database');
+
+        app.listen(3000, () => {
+            console.log('Server is running on port 3000!')
+        });
+    })
+    .catch((err) => {
+        console.log("Error connecting to the Database: ", err)
+    })
+
+
+
